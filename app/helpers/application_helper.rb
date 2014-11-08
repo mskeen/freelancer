e3e233905772e3eac9259e272dabc17e6ef0bcc6
Mike Skeen <skeen.mike@gmail.com>
2014-11-08 14:48:34 -0500

    Add Title handler

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def title(page_title)
+    provide(:title) do
+      if page_title.empty?
+        APP_CONFIG['site_name']
+      else
+        "#{page_title} - #{APP_CONFIG['site_name']}"
+      end
+    end
+  end
 end
