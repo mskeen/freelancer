6194ddeda236f932f5c41e2c6f1b66f148a04534
Mike Skeen <skeen.mike@gmail.com>
2014-11-24 14:34:12 -0500

    Config SMTP server

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -21,10 +21,23 @@ module Freelancer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.action_mailer.default_url_options = { host: Rails.application.secrets.host }
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:        Rails.application.secrets.smtp['address'],
+      port:           Rails.application.secrets.smtp['port'],
+      domain:         Rails.application.secrets.smtp['domain'],
+      authentication: Rails.application.secrets.smtp['authentication'],
+      user_name:      Rails.application.secrets.smtp['user_name'],
+      password:       Rails.application.secrets.smtp['password']
+    }
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_deliveries = true
   end
 end
