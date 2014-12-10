d4b26ef6de3e201716e86d871e5149c12be9a1a5
Mike Skeen <skeen.mike@gmail.com>
2014-12-10 17:50:43 -0500

    Add email text for event_tracker alerts/clears

--- app/mailers/event_tracker_mailer.rb
+++ app/mailers/event_tracker_mailer.rb
@@ -2,12 +2,14 @@ class EventTrackerMailer < ActionMailer::Base
   default from: AppConfig.support_email
 
   def alert(event_tracker)
+    @event_tracker = event_tracker
     mail to: event_tracker.emails.join(","),
-         subject: "Alert: #{event_tracker.name}"
+         subject: "Alert: #{@event_tracker.name}"
   end
 
   def alert_cleared(event_tracker)
+    @event_tracker = event_tracker
     mail to: event_tracker.emails.join(","),
-         subject: "Alert Cleared: #{event_tracker.name}"
+         subject: "Alert Cleared: #{@event_tracker.name}"
   end
 end
