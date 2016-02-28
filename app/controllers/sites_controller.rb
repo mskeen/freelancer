c287ea38bdefcec7ea9d2ff73109d24d4415b7ff
Mike Skeen <skeen.mike@gmail.com>
2016-02-28 17:39:58 -0500

    Add SitesController

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,55 @@
+class SitesController < ApplicationController
+  before_filter :authenticate_user!
+  respond_to :html
+
+  before_action :set_site, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @sites = current_user.sites.active.all
+  end
+
+  def show
+    respond_with(@site)
+  end
+
+  def new
+    @site = Site.new(contact_user_ids: [current_user.id])
+    respond_with(@site)
+  end
+
+  def edit
+  end
+
+  def create
+    @site = current_user.sites.new(site_params)
+    @site.contact_user_ids = params[:site][:contact_user_ids]
+    @site.status = Site.status(:pending)
+    @site.is_deleted = false
+    @site.organization = current_user.organization
+    @site.save
+    respond_with(@site)
+  end
+
+  def update
+    @site.contact_user_ids = params[:site][:contact_user_ids]
+    @site.update(site_params)
+    respond_with(@site)
+  end
+
+  def destroy
+    @site.update_attributes(is_deleted: true)
+    respond_with(@site, location: sites_path)
+  end
+
+  private
+
+  def set_site
+    @site = current_user.sites.active.find_by_token!(params[:id])
+  end
+
+  def site_params
+    params.require(:site).permit(
+      :name, :url, :host, :interval_cd, contact_user_ids: []
+    )
+  end
+end
