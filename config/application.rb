c2263bfdccf838929b74611c4c25ddec3ea22e9a
Mike Skeen <skeen.mike@gmail.com>
2016-02-29 17:03:45 -0500

    Add a redis config file

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module Freelancer
     config.time_zone = 'Eastern Time (US & Canada)'
 
     config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
