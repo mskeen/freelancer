3a8fc2dfceece95bd721b061dae5c618b0ef3e12
Mike Skeen <skeen.mike@gmail.com>
2016-03-03 11:05:38 -0500

    Gave up on the multi-threaded worker

--- app/workers/log_monitor_worker.rb
+++ app/workers/log_monitor_worker.rb
@@ -2,37 +2,35 @@ class LogMonitorWorker
   include Sidekiq::Worker
 
   IGNORE = [/.css$/, /.js$/, /.png$/, /.jpg$/, /.jpeg$/, /.gif$/, /.ico$/]
-  DEFAULT_TAIL_LENGTH = 10 * 60 # 10 minutes
+  DEFAULT_TAIL_LENGTH = 5 * 60 # 5 minutes
 
   def perform(log_monitor_id)
-    @log_monitor_id = log_monitor_id
     @mon = LogMonitor[log_monitor_id]
     return if @mon.status != "pending"
     @ctr = 1
-    @done = false
-    @cancelled = false
     @site = Site.where(token: @mon.site_id).first
-    @mon.update status: "processing"
-    Thread.new { check_monitor_status }
+    @mon.update(status: "processing")
     monitor_log_file(@site.access_log_cmd(@mon.log_type))
     puts "Done! #{@ctr} lines processed."
-    if @cancelled
-      @mon.update status: "cancelled"
-    else
-      @mon.update status: "complete"
-    end
+    @mon.update(status: "complete")
   end
 
   def monitor_log_file(cmd)
-    puts cmd
     IO.popen(cmd) do |io|
       io.each do |log_line|
         unless log_line.match(/internal dummy connection/)
           process_line(log_line)
         end
-        return if @done || @cancelled
+        return if job_done
       end
     end
+  end
+
+  def job_done
+    @mon = LogMonitor[@mon.id]
+    @mon.status == "cancelled" ||
+      (@mon.log_type == "tail" &&
+        ((Time.now.to_s(:db).to_datetime - @mon.created_at.to_datetime) * 24 * 60 * 60).to_i > DEFAULT_TAIL_LENGTH)
   end
 
   def process_line(log_line)
@@ -59,26 +57,22 @@ class LogMonitorWorker
   end
 
   def add_status_entry(ip, tm, msg)
-    log_ip = find_or_create_log_ip(ip)
-    log_ip.update(last_hit: tm.to_s(:db))
-    entry = LogEntry.create(logged_at: tm, status_msg: msg)
-    log_ip.log_entries.add(entry)
+    add_entry(ip, tm, nil, nil, nil, nil, nil, nil, msg)
   end
 
-  def add_entry(ip, tm, method, url, status, size, referrer, agent)
-    log_ip = find_or_create_log_ip(ip)
-    log_ip.update(last_hit: tm.to_s(:db), agent: agent)
-
+  def add_entry(ip, tm, method, url, status, size, referrer, agent, msg = nil)
+    log_ip = find_or_create_log_ip(ip, agent)
+    log_ip.update(last_hit: tm.to_s(:db))
     entry = LogEntry.create(logged_at: tm.to_s(:db), method: method,
                             url: url, status: status, size: size,
-                            referrer: referrer)
+                            referrer: referrer, status_msg: msg)
     log_ip.log_entries.add(entry)
   end
 
-  def find_or_create_log_ip(ip)
+  def find_or_create_log_ip(ip, agent)
     log_ip = @mon.log_ips.find(ip: ip).first
     if !log_ip
-      log_ip = LogIp.create(ip: ip)
+      log_ip = LogIp.create(ip: ip, agent: agent)
       @mon.log_ips.add(log_ip)
     end
     @mon.incr :hits
@@ -86,19 +80,4 @@ class LogMonitorWorker
     log_ip
   end
 
-  def check_monitor_status
-    loop do
-      sleep(1)
-      puts "Checking for cancelled status"
-      @mon = LogMonitor[@log_monitor_id]
-      if @mon.status == "cancelled"
-        @cancelled = true
-      end
-      if @mon.log_type == "tail" &&
-        ((Time.now.to_s(:db).to_datetime - mon.created_at.to_datetime) * 24 * 60 * 60).to_i > DEFAULT_TAIL_LENGTH
-        @done = true
-      end
-      break if @done || @cancelled
-    end
-  end
 end
